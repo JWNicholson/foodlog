728b9dd97c652e1c92d25a9398afe2de2fe62ad8
JWNicholson <john.nicholson812@gmail.com>
2020-11-13 12:48:19 -0500

    added database migrations,setup scaffolding,added bulma link

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # change root path so bootup goes to designated page instead of R on R welcome screen,
+  # - app/controllers/entries_controller.rb
+  root to: "entries#index"
 end
