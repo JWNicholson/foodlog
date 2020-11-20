a5e1ac91d6f0f9ddd599528cf6e5678b98258053
JWNicholson <john.nicholson812@gmail.com>
2020-11-20 13:26:55 -0500

    added archived entries view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'archive/index'
+  get 'archives/index'
   resources :entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
