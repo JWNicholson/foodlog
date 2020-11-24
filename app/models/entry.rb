049703b3168f290af74cf50fd9866dab5a839539
JWNicholson <john.nicholson812@gmail.com>
2020-11-24 14:08:16 -0500

    added validation to entry form to check if fields have content

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,5 @@
 class Entry < ApplicationRecord
+    validates :calories, :protiens, :carbohydrates, :fats, :meal_type, presence: true
     def day
         self.created_at.strftime("%m/%d/%Y")
     end
